7686e9d761c2e76db5b89b6860eb091f3d44c544
Udit Pai <uditpai2002@gmail.com>
2023-02-15 19:05:02 -0600

    deleted extra skills folder

--- skills/spin-up-skills/include/PositionTracker.h
+++
@@ -1,9 +0,0 @@
-#include "robot.h"
-
-extern double positionX;
-extern double positionY;
-const double RADIAL_TRACKING_WHEEL_OFFSET = 0.1;
-
-void PositionTracker(double transverseWheelRadMeters, double radialWheelRadMeters);
-void initTracker(double initial_X, double initial_Y);
-void updatePosition(double imu_heading);
