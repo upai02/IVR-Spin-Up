67af9359d25911bac4ee0016af851e1a1f9a50b5
Reid F <rf6846@gmail.com>
2023-02-15 19:09:13 -0600

    Default args for initTracker in PositionTracker

--- skills/spin-up-mingo/include/misc/PositionTracker.h
+++ skills/spin-up-mingo/include/misc/PositionTracker.h
@@ -5,5 +5,5 @@ extern double positionY;
 const double RADIAL_TRACKING_WHEEL_OFFSET = 0.1;
 
 void PositionTracker(double transverseWheelRadMeters, double radialWheelRadMeters);
-void initTracker(double initial_X, double initial_Y);
+void initTracker(double initial_X = 0.0, double initial_Y = 0.0);
 void updatePosition(double imu_heading);
