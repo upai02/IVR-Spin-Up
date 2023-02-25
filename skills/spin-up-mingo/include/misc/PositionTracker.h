74ced918bee2b45f5891e5f7817e940748bb770f
Reid F <rf6846@gmail.com>
2023-02-25 12:27:17 -0500

    Hopefully working odom thread change

--- skills/spin-up-mingo/include/misc/PositionTracker.h
+++ skills/spin-up-mingo/include/misc/PositionTracker.h
@@ -1,12 +1,17 @@
+#include "pros/rtos.hpp"
 #include "robot.h"
 
 extern double positionX;
 extern double positionY;
 const double RADIAL_TRACKING_WHEEL_OFFSET = 0.0;
+inline pros::Mutex positionX_mutex;
 
 void PositionTracker(double transverseWheelRadMeters, double radialWheelRadMeters);
 bool trackerInitialized();
 void initTracker(double initial_X = 0.0, double initial_Y = 0.0);
 void update_position();
+
+double getX();
+double getY();
 
 // extern pros::Task position_updater;
