4d679c62f2965054a4c1356b3c263582d91c3fe3
Reid F <rf6846@gmail.com>
2023-02-25 09:13:49 -0500

    Ready for path testing

--- skills/spin-up-mingo/src/auton.cpp
+++ skills/spin-up-mingo/src/auton.cpp
@@ -1,4 +1,5 @@
 #include "auton.h"
+#include "controls.h"
 #include "main.h"
 #include "pros/llemu.hpp"
 #include "pros/misc.h"
@@ -154,9 +155,10 @@ void boltSkillsAuto() {
 	std::vector<std::vector<double>> skillsPathSeg4 = {{skillsPathSeg3.back()}, {1.22, 1.83}, {0.7, 1.3}, {0.45, 2.11}}; // end facing goal (spin on spot)
 	std::vector<std::vector<double>> skillsPathSeg5 = {{skillsPathSeg4.back()}, {1.46, 2.26}}; // pick up 3 and drive back to center shooting spot again
 	// std::vector<std::vector<double>> skillsPathSeg6 = {{skillsPathSeg5.back()}, {1.9, 2.7}}; // go get first set of 3
-	std::vector<std::vector<double>> skillsPathSeg6 = {{skillsPathSeg5.back()}, {2.1, 2.7}}; // go get first set of 3	
+	std::vector<std::vector<double>> skillsPathSeg6 = {{skillsPathSeg5.back()}, {2.1, 2.7}}; // go get first set of 3
+    std::vector<std::vector<double>> skillsPathSeg7 = {{skillsPathSeg6.back()}, {1.55, 3.15}}; // go to shoot first set of 3
 	// std::vector<std::vector<double>> skillsPathSeg7 = {{skillsPathSeg6.back()}, {1.83, 3.05}}; // go closer and shoot first set of 3
-	std::vector<std::vector<double>> skillsPathSeg8 = {{skillsPathSeg6.back()}, {2.54, 2.7}}; // go get 2nd set of 3
+	std::vector<std::vector<double>> skillsPathSeg8 = {{skillsPathSeg7.back()}, {2.54, 2.7}}; // go get 2nd set of 3
 	std::vector<std::vector<double>> skillsPathSeg9 = {{skillsPathSeg8.back()}, {1.9, 2.9}}; // shoot 2nd set of 3
 	std::vector<std::vector<double>> skillsPathSeg10 = {{skillsPathSeg9.back()}, {2.1, 2.1}}; // get first solo on diagonal
 	std::vector<std::vector<double>> skillsPathSeg11 = {{skillsPathSeg10.back()}, {2.7, 2.7}, {1.8, 3.4}, {1.5, 3.356}}; // get 2nd on solo diagonal + back to drop spot
@@ -174,19 +176,26 @@ void boltSkillsAuto() {
 	// shootAndWait();
 	// shoot
 	followPath(skillsPathSeg4, calcGoalAngle(skillsPathSeg4.back()), true);
+    // shootAndWait();
 	// shoot
 	turnToAngle(270.0, 2.0);
 	followPath(skillsPathSeg5, calcGoalAngle(skillsPathSeg5.back()), true);
+    // shootAndWait();
 	// shoot
-	followPath(skillsPathSeg6, calcGoalAngle(skillsPathSeg6.back()), true);
-	// shoot
-	// followPath(skillsPathSeg7, calcGoalAngle(skillsPathSeg7.back()), false);
+	followPath(skillsPathSeg6, 60, true);
+	followPath(skillsPathSeg7, calcGoalAngle(skillsPathSeg7.back()), false);
+    turnToPoint();
+    // shootAndWait();
+    // shoot
+
+    /*
 	followPath(skillsPathSeg8, 270.0, true, true, 0.5, 45.0);
 	followPath(skillsPathSeg9, calcGoalAngle(skillsPathSeg9.back()), false);
 	// shoot
 	followPath(skillsPathSeg10, 320, true, true, 0.5, 45.0);
 	followPath(skillsPathSeg11, calcGoalAngle(skillsPathSeg11.back()), true); // 0.2 off on Y with these
-	// shoot
+    // shoot
+	*/
 	// SmartStop();
 	intake.move_voltage(0);
 	stopMotors();
